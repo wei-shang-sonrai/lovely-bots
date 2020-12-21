86c1ce2c8e18832c29c2744ffb6554d1af192a00
Wei Shang <wei.shang@Sonrai-WeiShang.local>
2020-12-21 14:27:08 -0400

    fix(bot): add debug message for parsing out arn

--- aws-ec2-start/bot.py
+++ aws-ec2-start/bot.py
@@ -1,7 +1,18 @@
 import botocore
-
+import sonrai.platform.aws.arn
 
 def run(ctx):
+
+    print("hello world!")
+
+
+    resource_id = ctx.resource_id
+    resource_arn = sonrai.platform.aws.arn.parse(resource_id)
+
+    print(resource_id)
+
+    print(resource_arn)
+
     # Create ec2 client 
     client = ctx.get_client().get('ec2')
     
